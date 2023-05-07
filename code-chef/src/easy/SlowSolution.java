701f7aaed1e0a753ea9dcde27d070d480f37bafb
subratasikdar <subrata.sikdar.freelancer@gmail.com>
2023-05-07 13:30:34 +0530

    problem slow soln from code chef

---
+++ code-chef/src/easy/SlowSolution.java
@@ -0,0 +1,30 @@
+package easy;
+
+import java.util.Scanner;
+
+/**
+ * https://www.codechef.com/problems/SLOWSOLN?tab=statement
+ */
+public class SlowSolution {
+
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner in = new Scanner(System.in);
+        int testcase = in.nextInt();
+
+        for (int t = 0; t < testcase; t++) {
+
+            int maxT = in.nextInt();
+            int maxN = in.nextInt();
+            int sumN = in.nextInt();
+
+            int quotient = sumN / maxN;
+
+            if (quotient >= maxT) { // if more than the given test case
+                System.out.println(maxT * maxN * maxN);
+            } else {
+                int remainder = sumN % maxN;
+                System.out.println(maxN * maxN * quotient + remainder * remainder);
+            }
+        }
+    }
+}

ca6c606df9d01c33cc7fe5f9fc46e2c062353159
subratasikdar <subrata.sikdar.freelancer@gmail.com>
2023-05-14 15:35:02 +0530

    medium problem solved from leetcode

---
+++ leet-code/src/medium/CompressStringCharByGroupLength.java
@@ -0,0 +1,31 @@
+package medium;
+
+public class CompressStringCharByGroupLength {
+
+    public static void main(String[] args) {
+
+        char[] chars = {'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'};
+        CompressStringCharByGroupLength byGroupLength = new CompressStringCharByGroupLength();
+        System.out.println(byGroupLength.compress(chars));
+    }
+
+    int compress(char[] chars) {
+        int i = 0, res = 0;
+
+        while (i < chars.length) {
+            int groupLength = 1;
+            while (i + groupLength < chars.length && chars[i + groupLength] == chars[i]) {
+                groupLength++;
+            }
+            chars[res++] = chars[i];
+            if (groupLength > 1) {
+                for (char c : Integer.toString(groupLength).toCharArray()) {
+                    chars[res++] = c;
+                }
+            }
+            i += groupLength;
+        }
+
+        return res;
+    }
+}
